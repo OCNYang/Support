8dd69d200c25560a32019ec49486a2181ea1d847
OCN.Yang <yangocn@163.com>
2018-03-06 09:38:41 +0800

    versioncode 3;v1.0.2;

--- uapplibrary/src/main/java/com/ocnyang/uapplibrary/UAppBaseFragmentAppCompatActivity.java
+++ uapplibrary/src/main/java/com/ocnyang/uapplibrary/UAppBaseFragmentAppCompatActivity.java
@@ -3,7 +3,7 @@ package com.ocnyang.uapplibrary;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
 /*******************************************************************
  *    * * * *   * * * *   *     *       Created by OCN.Yang
@@ -13,7 +13,7 @@ import android.support.v4.app.FragmentActivity;
  *******************************************************************/
 
 
-public class UAppBaseFragmentAppCompatActivity extends FragmentActivity {
+public class UAppBaseFragmentAppCompatActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
