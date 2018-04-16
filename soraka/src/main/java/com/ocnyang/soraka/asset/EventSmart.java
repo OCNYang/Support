f3ce71922112dbff92273566c60d7df349b36deb
OCN.Yang <yangocn@163.com>
2018-04-16 19:58:09 +0800

    添加新方式：防止过快点击造成多次事件执行；

---
+++ soraka/src/main/java/com/ocnyang/soraka/asset/EventSmart.java
@@ -0,0 +1,24 @@
+package com.ocnyang.soraka.asset;
+
+/*******************************************************************
+ *    * * * *   * * * *   *     *       Created by OCN.Yang
+ *    *     *   *         * *   *       Time:2018/4/16 19:47.
+ *    *     *   *         *   * *       Email address:ocnyang@gmail.com
+ *    * * * *   * * * *   *     *.Yang  Web site:www.ocnyang.com
+ *******************************************************************/
+
+public class EventSmart {
+    // 两次点击按钮之间的点击间隔不能少于1000毫秒
+    private static final int MIN_CLICK_DELAY_TIME = 1000;
+    private static long lastClickTime;
+
+    public static boolean click() {
+        boolean flag = false;
+        long curClickTime = System.currentTimeMillis();
+        if ((curClickTime - lastClickTime) >= MIN_CLICK_DELAY_TIME) {
+            flag = true;
+        }
+        lastClickTime = curClickTime;
+        return flag;
+    }
+}
